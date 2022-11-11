421432e536b9c3363715d22558c417a26f77e62e
Daniel Haydamous <dhaydamo@e2r8p4.clusters.42paris.fr>
2022-11-11 17:35:36 +0100

    fdsa

--- get_next_line.c
+++ get_next_line.c
@@ -47,16 +47,37 @@ void	lst_add_back(char *buffer, t_list **remainder)
 	(*remainder)->next = new;
 }
 
-void	separate_line(char *buffer, t_list **remainder)
+void	separate_line(char *buffer, t_list **remainder, int before_len, int after_len)
 {
-	char	*to_new_line;
+	char	*before_new_line;
 	char	*after_new_line;
+	int	i;
 
+	if (!buffer || !remainder)
+		return;
+	before_new_line = malloc(sizeof(char) * (before_len + 1);
+	if (!before_new_len)
+		return;
+	i = -1;
+	while (--i < before_len)
+		before_new_line[i] = buffer[i];
+	before_new_line[i] = 0;
+	after_new_line = malloc(sizeof(char) * (after_len + 1);
+	if (!after_new_len)
+		return;
+	i = -1;
+	while (--i < after_len)
+		after_new_line[i] = buffer[after_len + i];
+	after_new_line[aftr_len + i] = 0;
+	lst_add_back(before_new_line, remainder);
+	lst_add_back(after_new_line, remainder);
 }
 
 void	fill_remainder(int fd, t_list **remainder)
 {
 	char	*buffer;
+	int	before_len;
+	int	after_len;
 
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
@@ -67,7 +88,13 @@ void	fill_remainder(int fd, t_list **remainder)
 		if (!contains_new_line(buffer))
 			lst_add_back(buffer, remainder);
 	}
-	separate_line(buffer, remainder);
+	before_len = 1;
+	after_len = 0;
+	while (buffer[before_len] != '\n')
+		before_len++;
+	while (buffer[before_len + after_len])
+		after_len++;
+	separate_line(buffer, remainder, before_len, after_len);
 }
 
 char	*get_next_line(int fd)
@@ -76,9 +103,7 @@ char	*get_next_line(int fd)
 	char	*line;
 	static t_list	*remainder;
 
-	// 1. Read file until new line is found, addind to remainder.
 	fill_remainder(fd, &remainder);
-	// 2. Separate the line containing the new line, adding it to remainder
 	// 3. concatenate to get line
 	// 4. clean remainder
 
